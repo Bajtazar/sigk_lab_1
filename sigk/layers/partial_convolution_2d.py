f11ee7a3532837f95f49754a331b25e0566c197f
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-12 21:01:44 +0100

    Added preliminary partial conv2d impl

--- sigk/layers/partial_convolution_2d.py
+++ sigk/layers/partial_convolution_2d.py
@@ -1,5 +1,12 @@
 from torch.nn import Conv2d
-from torch import ones_like, tensor, dtype as tensor_dtype, device as tensor_device
+from torch import (
+    ones_like,
+    tensor,
+    dtype as tensor_dtype,
+    device as tensor_device,
+    Tensor,
+    no_grad,
+)
 
 from numpy import prod
 
@@ -20,6 +27,7 @@ class PartialConv2d(Conv2d):
         padding_mode: str = "zeros",
         device: Optional[tensor_device] = None,
         dtype: Optional[tensor_dtype] = None,
+        epsilon: float = 1e-8,
     ) -> None:
         super().__init__(
             in_channels=in_channels,
@@ -36,6 +44,25 @@ class PartialConv2d(Conv2d):
         )
         self.register_buffer("mask_coeffs", ones_like(self.weight))
         self.register_buffer(
-            "mask_norm_coeff",
+            "conv_window_size",
             tensor([prod(self.weight.shape[1:])], dtype=dtype, device=device),
         )
+        self.__epsilon = epsilon
+        self.bias = self.bias.view(1, self.bias.shape[0], 1, 1)
+
+    @property
+    def epsilon(self) -> float:
+        return self.__epsilon
+
+    @no_grad
+    def __calculate_current_mask(self, mask: Tensor) -> tuple[Tensor, float]:
+        current_mask = self._conv_forward(mask, self.mask_coeffs, bias=None)
+        ratio = self.conv_window_size / (current_mask + self.epsilon)
+        current_mask = current_mask.clamp(min=0, max=1)
+        return current_mask, current_mask * ratio
+
+    def forward(self, tensor: Tensor, mask: Tensor) -> tuple[Tensor, Tensor]:
+        mask, ratio = self.__calculate_current_mask(mask)
+
+        out = self._conv_forward(tensor * mask, self.weight, bias=None)
+        return (out * ratio) + self.bias, mask
