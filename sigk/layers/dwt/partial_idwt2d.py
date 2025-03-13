4be9649a481da97dc2cc53f2f2eca9c49a537918
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-13 20:42:51 +0100

    Added calculate pass masks method to partial idwt2d

--- sigk/layers/dwt/partial_idwt2d.py
+++ sigk/layers/dwt/partial_idwt2d.py
@@ -7,6 +7,7 @@ from torch import (
     ones_like,
     Tensor,
     tensor,
+    no_grad,
 )
 
 from numpy import prod
@@ -39,6 +40,39 @@ class DwtBase(IDwt2D):
             f"{name}_window_size",
             tensor([prod(kernel.shape[1:])], dtype=kernel.dtype, device=kernel.device),
         )
+
+    def __calculate_pass_mask(
+        self,
+        mask: Tensor,
+        pass_mask: Tensor,
+        pass_window_size: int,
+        position: int,
+        groups: int,
+    ) -> tuple[Tensor, Tensor]:
+        current_mask = self._perform_dwt_pass(
+            mask, kernel=pass_mask, position=position, groups=groups
+        )
+        ratio = pass_window_size / (current_mask + self.epsilon)
+        current_mask = current_mask.clamp(min=0, max=1)
+        return current_mask, current_mask * ratio
+
+    @no_grad
+    def __calculate_pass_masks(self, mask: Tensor) -> tuple[Tensor, Tensor, Tensor]:
+        second_pass_mask, second_pass_ratio = self.__calculate_pass_mask(
+            mask=mask,
+            pass_mask=self.second_pass_mask_coeffs,
+            pass_window_size=self.second_pass_window_size,
+            position=-1,
+            groups=2 * self.channels,
+        )
+        first_pass_mask, first_pass_ratio = self.__calculate_pass_mask(
+            mask=second_pass_mask,
+            pass_mask=self.first_pass_mask_coeffs,
+            pass_window_size=self.firstpass_window_size,
+            position=-2,
+            groups=self.channels,
+        )
+        return second_pass_ratio, first_pass_mask, first_pass_ratio
 
     @property
     def epsilon(self) -> float:
