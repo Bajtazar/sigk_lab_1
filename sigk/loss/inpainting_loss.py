af3ada860b317a62c98781d48c98723f61f1cd29
Bajtazar <grzegorz.czarnecki.2021@gmail.com>
2025-03-14 20:53:01 +0100

    Added total variation loss

--- sigk/loss/inpainting_loss.py
+++ sigk/loss/inpainting_loss.py
@@ -10,11 +10,14 @@ class InpaintingLoss(Module):
         valid_lambda: float = 1.0,
         hole_lambda: float = 6.0,
         smooth_lamda: float = 0.1,
+        epsilon: float = 1e-8,
     ) -> None:
         super().__init__()
         self.__valid_lambda = valid_lambda
         self.__hole_lambda = hole_lambda
         self.__smooth_lambda = smooth_lamda
+        self.__epsilon = epsilon
+        self.__embedding = None
 
     @property
     def valid_lambda(self) -> float:
@@ -28,6 +31,10 @@ class InpaintingLoss(Module):
     def smooth_lambda(self) -> float:
         return self.__smooth_lambda
 
+    @property
+    def epsilon(self) -> float:
+        return self.__epsilon
+
     def __get_composition(
         self, x: Tensor, x_hat: Tensor, mask: Tensor, hole_mask: Tensor
     ) -> Tensor:
@@ -39,11 +46,17 @@ class InpaintingLoss(Module):
     def __hole_loss(self, x: Tensor, x_hat: Tensor, hole_mask: Tensor) -> Tensor:
         return self.hole_lambda * abs(x * hole_mask - x_hat * hole_mask).mean()
 
+    def __total_variation_loss(self, composition: Tensor) -> Tensor:
+        horizontal_loss = abs(composition[..., :-1] - composition[..., 1:]).mean()
+        vertical_loss = abs(composition[..., :-1, :] - composition[..., 1:, :]).mean()
+        return self.smooth_lambda * (horizontal_loss + vertical_loss)
+
     def __call__(self, x: Tensor, x_hat: Tensor, mask: Tensor) -> Tensor:
         hole_mask = 1 - mask
         composition = self.__get_composition(x, x_hat, mask, hole_mask)
         valid_loss = self.__valid_loss(x, x_hat, mask)
         hole_loss = self.__hole_loss(x, x_hat, hole_mask)
+        total_variation_loss = self.__total_variation_loss(composition)
 
     def __calculate_gram_matrix(matrix: Tensor) -> Tensor:
         left = matrix.flatten(dim=-2)
