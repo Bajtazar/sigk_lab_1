552bafa065d4dea5599a98035717f95c27db153d
Grzegorz Tomasz Czarnecki <01158613@pw.edu.pl>
2025-03-15 14:36:35 +0100

    Updated partial gdn

--- sigk/layers/partial_gdn.py
+++ sigk/layers/partial_gdn.py
@@ -3,19 +3,11 @@ from sigk.layers.gdn import GDN, IGDN
 from torch import Tensor
 
 
-class PartialGDNBase:
-    def _calculate_mask(self, mask: Tensor) -> Tensor:
-        out_mask = mask.sum(dim=1).clamp(min=0, max=1).unsqueeze(1)
-        return out_mask.repeat(1, mask.shape[1], 1, 1)
-
-
-class PartialGDN(GDN, PartialGDNBase):
+class PartialGDN(GDN):
     def forward(self, tensor: Tensor, mask: Tensor) -> tuple[Tensor, Tensor]:
-        out_mask = self._calculate_mask(mask)
-        return super().forward(tensor * mask) * out_mask, out_mask
+        return super().forward(tensor * mask) * mask, mask
 
 
-class PartialIGDN(IGDN, PartialGDNBase):
+class PartialIGDN(IGDN):
     def forward(self, tensor: Tensor, mask: Tensor) -> tuple[Tensor, Tensor]:
-        out_mask = self._calculate_mask(mask)
-        return super().forward(tensor * mask) * out_mask, out_mask
+        return super().forward(tensor * mask) * mask, mask
