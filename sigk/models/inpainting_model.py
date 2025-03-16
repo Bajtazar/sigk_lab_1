5dd6956a812224d8c7618a29bebd4120b5b52ca2
Grzegorz Tomasz Czarnecki <01158613@pw.edu.pl>
2025-03-17 00:01:59 +0100

    Fixes

--- sigk/models/inpainting_model.py
+++ sigk/models/inpainting_model.py
@@ -3,7 +3,7 @@ from sigk.layers.spectral.partial_spectral_fused_mb_conv import (
     PartialSpectralFusedMBConv,
 )
 from sigk.layers.partial_gdn import PartialGDN, PartialIGDN
-from sigk.layers.partial_gelu import PartialGELU
+from sigk.layers.partial_leaky_relu import PartialLeakyReLU
 from sigk.layers.partial_multihead_attention import PartialMultiheadAttention
 from sigk.layers.dwt import (
     PartialAdaptiveDwt2D,
@@ -12,7 +12,7 @@ from sigk.layers.dwt import (
 )
 from sigk.utils.unpacking_sequence import UnpackingSequential
 
-from torch import Tensor
+from torch import Tensor, chunk, cat
 from torch.nn import Module, ParameterList
 
 
@@ -46,7 +46,7 @@ class SynthesisConvolutionBlock(Module):
         )
         self.__sequence = UnpackingSequential(
             PartialSpectralConv2d(in_channels, in_channels, kernel_size=3, padding=1),
-            PartialGELU(),
+            PartialLeakyReLU(),
             PartialIGDN(channels=in_channels),
             PartialSpectralConv2d(in_channels, out_channels, kernel_size=3, padding=1),
         )
@@ -95,10 +95,10 @@ class SynthesisFusedBlock(Module):
     def __init__(self, in_channels: int, out_channels: int) -> None:
         super().__init__()
         self.__sequence = UnpackingSequential(
-            PartialSpectralFusedMBConv(in_channels, in_channels),
-            PartialGELU(),
+            PartialSpectralFusedMBConv(in_channels),
+            PartialLeakyReLU(),
             PartialIGDN(channels=in_channels),
-            PartialSpectralFusedMBConv(in_channels, in_channels),
+            PartialSpectralFusedMBConv(in_channels),
             PartialSpectralConv2d(
                 in_channels, out_channels, kernel_size=2, padding=1, groups=out_channels
             ),
@@ -116,6 +116,42 @@ class SynthesisFusedBlock(Module):
     ) -> tuple[Tensor, Tensor]:
         pre_recon, pre_recon_mask = self.__idwt((ll, *bands), (ll_mask, *bands_masks))
         return self.__sequence(pre_recon, pre_recon_mask)
+
+
+class Bottleneck(Module):
+    def __init__(self, channels: int, heads: int, latent_size: int) -> None:
+        super().__init__()
+        self.__stem = UnpackingSequential(
+            PartialSpectralConv2d(channels, 2 * channels, kernel_size=1),
+            PartialGDN(2 * channels),
+        )
+        self.__low_pass = UnpackingSequential(
+            PartialMultiheadAttention(
+                channels=channels,
+                heads=heads,
+                channels_per_head=channels // heads,
+                latent_size=latent_size,
+            ),
+        )
+        self.__high_pass = UnpackingSequential(
+            PartialSpectralConv2d(2 * channels, 2 * channels, kernel_size=3, padding=1),
+            PartialGDN(2 * channels),
+            PartialLeakyReLU(),
+            PartialSpectralConv2d(2 * channels, channels, kernel_size=3, padding=1),
+            PartialLeakyReLU(),
+            PartialIGDN(channels),
+        )
+        self.__channels = channels
+
+    def forward(self, tensor: Tensor, mask: Tensor) -> tuple[Tensor, Tensor]:
+        stem_tensor, stem_mask = self.__stem(tensor, mask)
+        low_pass, low_pass_mask = self.__low_pass(
+            stem_tensor[:, self.__channels :, ...],
+            stem_mask[:, self.__channels :, ...],
+        )
+        tensor = cat((low_pass, stem_tensor[:, : self.__channels, ...]), dim=1)
+        mask = cat((low_pass_mask, stem_mask[:, : self.__channels, ...]), dim=1)
+        return self.__high_pass(tensor, mask)
 
 
 class InpaintingModel(Module):
@@ -138,15 +174,8 @@ class InpaintingModel(Module):
                 AnalysisFusedBlock(8 * embedding_features, 16 * embedding_features),
             ]
         )
-        self.__low_level_recon = UnpackingSequential(
-            PartialGDN(channels=16 * embedding_features),
-            PartialMultiheadAttention(
-                channels=16 * embedding_features,
-                heads=attention_heads,
-                channels_per_head=16 * embedding_features // attention_heads,
-                latent_size=latent_size,
-            ),
-            PartialIGDN(16 * embedding_features),
+        self.__low_level_recon = Bottleneck(
+            16 * embedding_features, attention_heads, latent_size
         )
         self.__synthesis_blocks = ParameterList(
             [
